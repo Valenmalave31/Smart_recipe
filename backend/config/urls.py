e5d0aee7ecee6a3df68da0e3ec0b574704c6715d
Valentina Rojas <141515484+Valenmalave31@users.noreply.github.com>
2026-06-12 15:34:46 -0500

    fix: configurar variables de entorno y actualizar .gitignore

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from core import views
+from Back.Front.core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
